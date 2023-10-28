88185d3cb2a108a6eab3537f51dddd194fa6ad21
NiFink <Nils@team-fink.de>
2023-10-28 16:00:13 +0200

    Scenebuilder integrate

--- src/main/java/mainpackage/Controller.java
+++ src/main/java/mainpackage/Controller.java
@@ -3,10 +3,18 @@ package mainpackage;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class Controller {
     @FXML
     private Button btLogin;
+    @FXML
+    private Button btRegister;
+    @FXML
+    private TextField tbUsername;
+    @FXML
+    private PasswordField pbPassword;
     @FXML
     private void sayHello(ActionEvent event) {
 
