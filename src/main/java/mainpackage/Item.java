e4e33a2698bebc6c05e56de028d49846b70b37ce
Jan <js485@hdm-stuttgart.de>
2023-10-29 13:43:30 +0100

    added JSON file added json.simple library

--- src/main/java/mainpackage/Item.java
+++ src/main/java/mainpackage/Item.java
@@ -1,4 +1,13 @@
 package mainpackage;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Item {
 
@@ -10,7 +19,7 @@ public class Item {
     private String description;
     private boolean favourite;
 
-    public Item(int id, String name, float price, float weight, int available, String description, boolean favourite) {
+    public Item(int id, String name, float price, float weight, int available, String description, boolean favourite) throws IOException, ParseException {
         this.id = id;
         this.name = name;
         this.price = price;
