34df161c0a82927ac8ddca8bd8480180b8bce56c
Jan <js485@hdm-stuttgart.de>
2023-10-30 12:27:10 +0100

    implemented updateData() Method in class Item

--- src/main/java/mainpackage/Item.java
+++ src/main/java/mainpackage/Item.java
@@ -6,6 +6,7 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -19,14 +20,22 @@ public class Item {
     private String description;
     private boolean favourite;
 
+    JSONParser parser = new JSONParser();
+    JSONObject itemData = (JSONObject) parser.parse(new FileReader("src/main/resources/itemData.json"));
+
     public static void main(String[] args) throws IOException, ParseException {
         Item item = new Item("apfel");
         System.out.println(item.toString());
+        item.favor();
+        item.updateData();
+        System.out.println(item.toString());
     }
 
+    /**
+     * Konstruktor erstellt Item anhand des Parameter name und holt sich die dazugehörigen Daten aus der itemData.json Datei
+     * @param name Name des Items das erstellt wird
+     */
     public Item(String name) throws IOException, ParseException {
-        JSONParser parser = new JSONParser();
-        JSONObject itemData = (JSONObject) parser.parse(new FileReader("src/main/resources/itemData.json"));
         JSONObject item = (JSONObject) itemData.get(name);
 
         this.id = (Long) item.get("id");
@@ -38,14 +47,29 @@ public class Item {
         this.favourite = (Boolean) item.get("favourite");
     }
 
-    private void select(){
+    private void select(){    }
 
-    }
+    /**
+     * Inverts the boolean favourite
+     */
     private void favor(){
-        favourite = true;
+        favourite = !favourite;
     }
-    private void showDetails(){
+    private void showDetails(){    }
 
+    /**
+     *  Updates Data in the json file (currently only favourite)
+     */
+    private void updateData() throws IOException {
+        JSONObject item = (JSONObject) itemData.get(this.name);
+        if((Boolean) item.get("favourite") != favourite){
+            item.put("favourite", true);
+            itemData.put(name, item);
+            try(FileWriter file = new FileWriter("src/main/resources/itemData.json")){
+                file.write(itemData.toString());
+                file.close();
+            }
+        }
     }
 
     @Override
