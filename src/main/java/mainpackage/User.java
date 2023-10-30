7be676441bb9e2102129edb61dcf56b7ad123d76
Michelle <mp180@hdm-stuttgart.de>
2023-10-30 14:00:54 +0100

    user registration method

--- src/main/java/mainpackage/User.java
+++ src/main/java/mainpackage/User.java
@@ -1,5 +1,16 @@
 package mainpackage;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+
 public class User {
 
 
@@ -11,14 +22,65 @@ public class User {
     private String password;
 
     private Item[] favourites;
+    static JSONParser parser = new JSONParser();
 
 
-    public void User(String userEmail, String userName, String password){
+
+    public static void main(String[] args) throws IOException, ParseException {
+        User michelle = new User("Michelle@gmail.com", "Michelle" , "1234");
+        registerNewUser(michelle);
+    }
+
+
+    public User(String userEmail, String userName, String password) throws IOException, ParseException {
 
         this.userEmail = userEmail;
         this.userName = userName;
         this.password = password;
 
+    }
+
+    public static void registerNewUser(User user){
+
+        try {
+
+            JSONObject newUser = new JSONObject();
+
+            newUser.put("userName", user.userName);
+            newUser.put("userEmail", user.userEmail);
+            newUser.put("password", user.password);
+
+
+
+            // TODO: evade doubling userNames
+
+
+            JSONObject userData = (JSONObject) parser.parse(new FileReader("src/main/resources/userData.json"));
+            userData.put(user.userName, newUser);
+            FileWriter fileWriter = new FileWriter("src/main/resources/userData.json");
+            fileWriter.write(userData.toJSONString() + "\n");
+            fileWriter.flush();
+
+            System.out.println("added new user: " + user);
+
+
+        }
+        catch (Exception e){
+            System.out.println(e);
+
+        }
+
+
+
+
+
+    }
+
+    @Override
+
+    public String toString(){
+
+        return "\n user: \n name: " + userName + "\n email: " + userEmail + "\n password: " + password;
     }
 
 
