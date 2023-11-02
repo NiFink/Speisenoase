60181db5a0a8376fc2bf49add78a49f4b630ee30
NiFink <Nils@team-fink.de>
2023-11-02 11:49:29 +0100

    Scene: Mainpage, added pruduxt

---
+++ src/main/java/mainpackage/ControllerMainpage.java
@@ -0,0 +1,60 @@
+package mainpackage;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ControllerMainpage {
+    @FXML
+    private Button btVegetables;
+    @FXML
+    private Button btFruit;
+    @FXML
+    private Button btSweeties;
+    @FXML
+    private Button btWine;
+    @FXML
+    private Button btBeer;
+    @FXML
+    private Button btLiquir;
+
+
+    @FXML
+    protected void checkBtVegetablesClick(ActionEvent event) {
+        System.exit(0);
+    }
+    @FXML
+    protected void checkBtFruitClick(ActionEvent event) {
+        System.exit(0);
+    }
+    @FXML
+    protected void checkBtSweetiesClick(ActionEvent event) {
+        System.exit(0);
+    }
+    @FXML
+    protected void checkBtWineClick(ActionEvent event) {
+        System.exit(0);
+    }
+    @FXML
+    protected void checkBtBeerClick(ActionEvent event) {
+        System.exit(0);
+    }
+    @FXML
+    protected void checkBtLiquirClick(ActionEvent event) {
+        System.exit(0);
+    }
+
+
+
+    @FXML
+    protected void checkExitClick(ActionEvent event) {
+        System.exit(0);
+    }
+}
