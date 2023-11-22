4a22f63184e2c4e4f9b760dae1a6d4e43174db46
NiFink <Nils@team-fink.de>
2023-11-22 17:13:36 +0100

    Scene: Register

--- src/main/java/mainpackage/ControllerLogin.java
+++ src/main/java/mainpackage/ControllerLogin.java
@@ -8,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
@@ -20,12 +22,14 @@ public class ControllerLogin {
     private PasswordField pbPassword;
 
     private UserManager userManager = new UserManager();
+    private static Logger log = LogManager.getLogger(ItemManager.class);
     @FXML
     protected void checkbtLoginClick(ActionEvent event) throws IOException {
         //TODO: If abfrage ob das Passwort stimmt mit der Datenbank eintrag überein
 
         if(tbUsername.getText().isBlank() == false && pbPassword.getText().isBlank() == false && userManager.userLogin(tbUsername.getText(), pbPassword.getText())){
             try {
+                log.info("User: " + tbUsername.getText() + " has logged in");
                 FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("MainPage.fxml"));
                 Scene scene = new Scene(fxmlLoader.load(), 860, 550);
                 Stage stage = Main.getStage();
@@ -38,6 +42,7 @@ public class ControllerLogin {
             }
         }
         else {
+            log.info("The user is attempting to log in with incorrect information.");
             lbLoginfailed.setText("Please enter a correct username and password.");
         }
 
