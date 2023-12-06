a485b725bafe583b0a84bdaf559ea286e8f44694
Jan <js485@hdm-stuttgart.de>
2023-12-06 15:05:40 +0100

    updated ItemManager

--- src/main/java/mainpackage/itempackage/Grocery.java
+++ src/main/java/mainpackage/itempackage/Grocery.java
@@ -44,7 +44,9 @@ class Grocery implements Item{
         this.id = id;
     }
 
-    public String getName() {return name; }
+    public String getName() {
+        return name;
+    }
 
     public void setName(String name) {
         this.name = name;
--- src/main/java/mainpackage/itempackage/ItemManager.java
+++ src/main/java/mainpackage/itempackage/ItemManager.java
@@ -9,36 +9,54 @@ import java.util.List;
 
 public class ItemManager {
 
-    private ArrayList<Item> items = getItems(15);
-    private ArrayList<ItemNode> itemNodes = getItemNodes(items);
-    private ArrayList<StackPane> itemPanes = getItemPanes(itemNodes);
+    private ArrayList<Item> items;
+    private ArrayList<ItemNode> itemNodes;
+    private ArrayList<StackPane> itemPanes;
     private ArrayList<String> favorites = new ArrayList<String>();
     private static final Logger log = LogManager.getLogger(ItemManager.class);
 
-    public ArrayList<Item> getItems(int id) {
-        items = new ArrayList<>();
+    public ItemManager(){
+        setItems(15);
+        setItemNodes(this.items);
+        setItemPanes(this.itemNodes);
+    }
+
+    private void setItems(int id) {
+        ArrayList<Item> items = new ArrayList<>();
         for (int i = 0; i < id; i++) {
             items.add(ItemFactory.createItem(ItemType.GROCERY, i));
         }
         log.info("List with " + items.size() + " items was created");
-        return items;
+        this.items = items;
     }
 
-    public ArrayList<ItemNode> getItemNodes(List<Item> items) {
-        itemNodes = new ArrayList<>();
+    private void setItemNodes(List<Item> items) {
+        ArrayList<ItemNode> itemNodes = new ArrayList<>();
         for (int i = 0; i < items.size(); i++) {
             itemNodes.add(new ItemNode(items.get(i)));
         }
         log.info("List with " + items.size() + " Stackpanes of Items was created");
+        this.itemNodes = itemNodes;
+    }
+
+    private void setItemPanes(ArrayList<ItemNode> itemNodes) {
+        ArrayList<StackPane> itemPanes = new ArrayList<>();
+        for (int i = 0; i < items.size(); i++) {
+            itemPanes.add(itemNodes.get(i).getItemPane());
+        }
+        log.info("List with " + items.size() + " Stackpanes of Items was created");
+        this.itemPanes = itemPanes;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public ArrayList<ItemNode> getItemNodes() {
         return itemNodes;
     }
 
-    public ArrayList<StackPane> getItemPanes(ArrayList<ItemNode> itemNodes) {
-        itemPanes = new ArrayList<>();
-        for (int i = 0; i < items.size(); i++) {
-            itemPanes.add(itemNodes.get(i).getItemNode());
-        }
-        log.info("List with " + items.size() + " Stackpanes of Items was created");
+    public ArrayList<StackPane> getItemPanes() {
         return itemPanes;
     }
 
@@ -47,6 +65,10 @@ public class ItemManager {
         for (int i = 0; i < items.size(); i++) {
             if (category.equalsIgnoreCase("all") || items.get(i).getCategory().toLowerCase().equals(category)) {
                 flowPane.getChildren().add(itemPanes.get(i));
+            } else if (category.equalsIgnoreCase("favs")){
+                if(getFavorites().contains(items.get(i).getName())){
+                    flowPane.getChildren().add(itemPanes.get(i));
+                }
             }
         }
         flowPane.setStyle("-fx-background-color:  #022235");
@@ -64,17 +86,6 @@ public class ItemManager {
         return flowPane;
     }
 
-    public FlowPane getItempaneFavs() {
-        FlowPane flowPane = new FlowPane();
-        for (int i = 0; i < items.size(); i++) {
-            if (getFavorites().contains(items.get(i).getName())) {
-                flowPane.getChildren().add(itemPanes.get(i));
-            }
-        }
-        flowPane.setStyle("-fx-background-color:  #022235");
-        return flowPane;
-    }
-
     public ArrayList<String> getFavorites() {
         for(int i = 0; i < items.size(); i++){
             if(itemNodes.get(i).isFavorite()){
@@ -83,4 +94,5 @@ public class ItemManager {
         }
         return favorites;
     }
+
 }
