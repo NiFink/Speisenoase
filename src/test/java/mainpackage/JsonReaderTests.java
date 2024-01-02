7b6ce92ca1e963c472744d436c78b50af1515c28
Jan <js485@hdm-stuttgart.de>
2024-01-02 14:33:48 +0100

    implemented JsonReaderTests

---
+++ src/test/java/mainpackage/JsonReaderTests.java
@@ -0,0 +1,24 @@
+package mainpackage;
+
+import mainpackage.itempackage.JsonReader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonReaderTests {
+
+    JsonReader jsonReader = JsonReader.getInstance();
+    private static final Logger log = LogManager.getLogger(JsonReaderTests.class);
+    @Test
+    public void ListNotNullTest() {
+        jsonReader.start();
+        jsonReader.start();
+        try {
+            jsonReader.join();
+        } catch (InterruptedException e) {
+            log.error(e.getMessage());
+        }
+        assertNotNull(jsonReader.getItemList());
+    }
+}
